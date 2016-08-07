689b787f9cf687585097a5dc0f106b36c46cd226
HardDie <oleg1995sysoev@yandex.ru>
2016-08-07 17:06:41 +0700

    Добавлены две функции для Windows, для перевода символа из нижнего регистра в верхний считанного с консоли и из файла

--- Main.cpp
+++ Main.cpp
@@ -629,26 +629,61 @@ int	test_typing_mix() {
 	return 0;
 		}
 
-char charTranslateW( const char symb ) {
-	char symb_ = symb;
-	// lowcase
-	if ( symb_ <= -91 && symb_ >= -96 || symb_ <= -81 && symb_ >= -90 ) {
-		return symb_ + 64;
-	} else if ( symb_ == -15 ) {
-		return -72;
-	} else if ( symb_ <= -17 && symb_ >= -32 ) {
-		return symb_ + 16;
+char CharTranslateW_Console( const char symb ) {
+	// Если на вход поступила английская буква нижнего региста, то просто возвращаем ее
+	if ( symb >= 97 && symb <= 122 ) {
+		return symb;
 	}
 
-	// highcase
-	if ( symb_ <= -123 && symb_ >= -128 || symb_ <= -97 && symb_ >= -122 ) {
-		return symb_ + 96;
-	} else if ( symb_ == -16 ) {
-		return -72;
-	} else if ( symb_ <= -17 && symb_ >= -32 ) {
-		return symb_ + 16;
+	// Перевод английского символа, верхнего регистра в нижний
+	if ( symb >= 65 && symb <= 90 ) {
+		return symb + 32;
 	}
-	return tolower( symb_ );
+
+	// Перевод введенной русской буквы нижнего регистра
+	if ( symb >= -96 && symb <= -91 || symb >= -90 && symb <= -81 ) {	// а-е ж-п
+		return symb + 64;
+	} else if ( symb == -15 ) {		// ё
+		return -72;
+	} else if ( symb >= -32 && symb <= -17 ) {	// р-я
+		return symb + 16;
+	}
+
+	// Перевод введенной русской буквы верхнего регистра
+	if ( symb >= -128 && symb <= -123 || symb >= -122 && symb <= -97 ) {	// А-Е Ж-Я
+		return symb + 96;
+	} else if ( symb == -16 ) {	// Ё
+		return -72;
+	}
+
+	return symb;
+}
+
+char CharTranslateW_File( const char symb ) {
+
+	// Если на вход поступила русская буква нижнего регистра, то просто возвращаем ее
+	if ( symb >= -32 && symb <= -1 || symb == -72 ) {
+		return symb;
+	}
+
+	// Если на вход поступила английская буква нижнего региста, то просто возвращаем ее
+	if ( symb >= 97 && symb <= 122 ) {
+		return symb;
+	}
+
+	// Перевод английского символа, верхнего регистра в нижний
+	if ( symb >= 65 && symb <= 90 ) {
+		return symb + 32;
+	}
+
+	// Перевод русской буквы верхнего регистра в нижний
+	if ( symb >= -64 && symb <= -33 ) {
+		return symb + 32;
+	} else if ( symb == -88 ) {	// Буква Ё
+		return -72;
+	}
+
+	return symb;
 }
 
 char charTranslateL( const char symb ) {
